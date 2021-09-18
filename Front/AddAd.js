1aaa663cf4f3f3469d2cbcaa37071abde8bd0971
Kerollos <kcok2014@gmail.com>
2021-09-18 20:33:26 +0200

    Kerollos

--- Front/AddAd.js
+++ Front/AddAd.js
@@ -1,3 +1,4 @@
+
 var firebaseConfig = {
     apiKey: "AIzaSyAuRCwuZPB4s43vBNhuv8A7jWNyD3qQUKU",
     authDomain: "sakkenyproject.firebaseapp.com",
@@ -13,6 +14,381 @@ var db = firebase.database();
 var stor = firebase.storage();
 var number = 0;
 var counter = 0;
+function setLoc(){
+  var cairo = ["Abbassia", "Ain Shams", "Azbakeya", "Bab al-Louq", "Boulaq", "City of the Dead (Cairo)", "Coptic Cairo", "Daher", "Downtown", "El Manial", "El Marg", "El Matareya", "El Qobbah", "El Rehab", "El Sahel", "El Sakkakini", "Ezbet El Haggana", "Ezbet El Nakhl", "Faggala", "Fifth Settlement", "Fustat", "Garden City", "Gezira", "Heliopolis", "Islamic", "Maadi", "Old Cairo", "Roda Island", "Shubra", "Shubra El Kheima", "Wagh El Birket", "Zamalek", "Zeitoun"];
+  var alex = ["Al Hadrah Qebli", "Amreya", "Anfoushi", "Asafra", "Azarita", "Bahary", "Bakos", "Baucalis", "Bolkly", "Camp Chezar", "Cleopatra", "Dekhela", "Downtown", "El Atareen", "El Gomrok", "El Ibrahimiya", "El Labban", "El Maamora", "El Mandara", "El Mansheya", "El Max", "El Qabary", "El Saraya", "El Soyof", "Fleming", "Gianaclis", "Glim", "Kafr Abdu", "Karmoz", "Kom El Deka", "Louran", "Mahatet El Raml", "Miami", "Moharam Bek", "Montaza", "Roshdy", "Saba Pasha", "Safar", "San Stefano", "Shatby", "Shods", "Sidi Bishr", "Sidi Gaber", "Smouha", "Sporting", "Stanley", "Tharwat", "Victoria", "Wardeyan", "Zezenia"];
+  var giza = ["Agouza", "Dokki", "Imbaba", "Mohandiseen"];
+  var beheira = ["Abu Hummus", "Abu El Matamir", "Damanhur", "Edku", "El Delengat", "El Mahmoudiyah", "El Rahmaniya", "Itay El Barud", "Hosh Issa", "Kafr El Dawwar", "Koum Hamada", "Rosetta", "Shubrakhit", "Wadi El Natrun", "El Nubaria"];
+  var kafr = ["El Hamool", "Baltim", "Biyala", "Desouk", "Fuwwah", "Kafr El Sheikh", "Metoubes", "Qallin", "El Reyad", "Sidi Salem"];
+  var port = ["El Dawahy", "El Arab", "El Ganoub", "El Manakh", "El Manasra", "El Sharq", "El Zohur", "Port Fuad", "Mubarak"];
+  var Sinai = ["El Arish", "El Hassana", "Sheikh Zuweid", "Bir El Abd", "Nakhl", "Rafah", "El Qasima", "Rumana"];
+  var gharb = ["El Mahalla El Kubra", "Kafr El Zayat", "Samanoud", "Tanta", "Zifta", "El Santa", "Kotoor", "Basyoun"];
+  var mnofia = ["Shibin El Kom", "Menouf", "Ashmoun", "Sers El Lyan", "Tala", "El Bagour", "El Shohada", "Sadat City", "Quesna", "Birket El Sab", "Shanawan"];
+  var qalyobia = ["Banha", "Khanka", "Qaha", "Qalyub", "Shibin El Qanater", "Shubra El Kheima", "Tukh", "El Qanater El Khayreya", "Kafr Shukr", "Obour City", "Khusus"];
+  var sharqia = ["10th of Ramadan", "Abu Hammad", "Abu Kebir", "Awlad Saqr", "Bilbeis", "Diyarb Negm", "El Husseiniya", "El Ibrahimiya", "El Qurein", "Faqous", "Hihya", "Kafr Saqr", "Mashtool El Souk", "Minya El Qamh", "El Salheya El Gedida", "Zagazig"];
+  var ismailia = ["Abu Suwir El Mahata", "Ismalia", "El Qantara", "El Qantara El Sharqiya", "New Kasaseen", "Tell El Kebir", "Fayid"];
+  var fayom = ["Ibsheway", "Itsa", "New Faiyum", "Sinnuris", "Tamiya", "Yousef El Seddik", "Kom Oshim", "New Kom Oshim", "Kouta"];
+  var suez = ["Arbaeen", "Ganayen", "Suez", "Attaka", "Faisal", "Port Suez"];
+  var domiat = ["El Zarqa", "Faraskur", "Port of Damietta", "Kafr El Battikh", "Kafr Saad", "New Damietta", "Ras El Bar"];
+  var baniswef = ["El Fashn", "El Wasta", "Biba", "Ihnasiya", "New Beni Suef", "Nasser", "Sumusta El Waqf"];
+  var minya = ["Abu Qirqas", "El Idwa", "Beni Mazar", "Deir Mawas", "New Minya", "Maghagha", "Malawit Gharb", "Mallawi", "Matai", "Samalut"];
+  var newVally = ["Kharga", "Balat", "Dakhla", "Farafra", "Baris", "Dakhla"];
+  var asyot = ["Abnub", "Abu Tig", "El Badari", "El Fath", "El Ghanayem", "El Qusiya", "Dairut", "New Asyut", "Manfalut", "Sahel Selim", "Sodfa"];
+  var redSea = ["Hurghada", "El Qusair", "Shalateen", "Halaib", "Marsa Alam", "Ras Gharib", "Safaga", "Bernes", "Alaki"];
+  var sohag = ["Akhmim", "El Balyana", "El Kawtar", "El Maragha", "El Munsha", "Aserat", "Dar El Salam", "Girga", "Juhaynah West", "New Akhmim", "New Sohag", "Saqultah", "Tahta", "Tima"];
+  var qena = ["Abu Tesht", "El Waqf", "Dishna", "Farshut", "New Qena", "Nag Hammadi", "Naqada", "Gebtu or Coptos", "Qus"];
+  var luxor = ["Qurna", "Armant", "Esna", "Tiba", "El Boghdadi", "New Tiba"];
+  var aswan = ["Abu Simbel", "Daraw", "Edfu", "Kom Ombo", "New Aswan", "New Tushka", "Nasser City", "El Basaliya", "El Radisia", "New Kalabsha", "Sebaiya"];
+  var matroh = ["El Dabaa", "El Alamein", "El Alameins", "El Hamam", "El Negaila", "Sallum", "North Coast", "Sidi Barrani", "Siwa Oasis"];
+  var daka = ["Aga", "El Gamaliya", "El Kurdi", "El Mansoura", "El Manzala", "El Matareya", "El Senbellawein", "Beni Ebeid", "Belqas", "Dikirnis", "Gamasa", "Maḥallat Damanah", "Minyet El Nasr", "Mit Ghamr", "Mit Salsil", "Nabaroh", "Shirbin", "Talkha", "Timay El Imdid"];
+  let city = document.getElementById("cities").value;
+  let sel = document.getElementById("locations");
+  if(city == ""){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+  }
+  if(city == "Cairo"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< cairo.length; i++){
+      var opt = document.createElement('option');
+      opt.value = cairo[i];
+      opt.innerHTML = cairo[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Alexandria"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< alex.length; i++){
+      var opt = document.createElement('option');
+      opt.value = alex[i];
+      opt.innerHTML = alex[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Giza"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< giza.length; i++){
+      var opt = document.createElement('option');
+      opt.value = giza[i];
+      opt.innerHTML = giza[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Beheira"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< beheira.length; i++){
+      var opt = document.createElement('option');
+      opt.value = beheira[i];
+      opt.innerHTML = beheira[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Kafr El Sheikh"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< kafr.length; i++){
+      var opt = document.createElement('option');
+      opt.value = kafr[i];
+      opt.innerHTML = kafr[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Port Said"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< port.length; i++){
+      var opt = document.createElement('option');
+      opt.value = port[i];
+      opt.innerHTML = port[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "North Sinai" || city == "South Sinai"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< Sinai.length; i++){
+      var opt = document.createElement('option');
+      opt.value = Sinai[i];
+      opt.innerHTML = Sinai[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Gharbia"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< gharb.length; i++){
+      var opt = document.createElement('option');
+      opt.value = gharb[i];
+      opt.innerHTML = gharb[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Monufia"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< mnofia.length; i++){
+      var opt = document.createElement('option');
+      opt.value = mnofia[i];
+      opt.innerHTML = mnofia[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Qalyubia"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< qalyobia.length; i++){
+      var opt = document.createElement('option');
+      opt.value = qalyobia[i];
+      opt.innerHTML = qalyobia[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Sharqia"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< sharqia.length; i++){
+      var opt = document.createElement('option');
+      opt.value = sharqia[i];
+      opt.innerHTML = sharqia[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Ismailia"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< ismailia.length; i++){
+      var opt = document.createElement('option');
+      opt.value = ismailia[i];
+      opt.innerHTML = ismailia[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Faiyum"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< fayom.length; i++){
+      var opt = document.createElement('option');
+      opt.value = fayom[i];
+      opt.innerHTML = fayom[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Suez"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< suez.length; i++){
+      var opt = document.createElement('option');
+      opt.value = suez[i];
+      opt.innerHTML = suez[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Damietta"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< domiat.length; i++){
+      var opt = document.createElement('option');
+      opt.value = domiat[i];
+      opt.innerHTML = domiat[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Beni Suef"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< baniswef.length; i++){
+      var opt = document.createElement('option');
+      opt.value = baniswef[i];
+      opt.innerHTML = baniswef[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Minya"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< minya.length; i++){
+      var opt = document.createElement('option');
+      opt.value = minya[i];
+      opt.innerHTML = minya[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "New Valley"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< newVally.length; i++){
+      var opt = document.createElement('option');
+      opt.value = newVally[i];
+      opt.innerHTML = newVally[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Asyut"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< asyot.length; i++){
+      var opt = document.createElement('option');
+      opt.value = asyot[i];
+      opt.innerHTML = asyot[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Red Sea"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< redSea.length; i++){
+      var opt = document.createElement('option');
+      opt.value = redSea[i];
+      opt.innerHTML = redSea[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Sohag"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< sohag.length; i++){
+      var opt = document.createElement('option');
+      opt.value = sohag[i];
+      opt.innerHTML = sohag[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Qena"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< qena.length; i++){
+      var opt = document.createElement('option');
+      opt.value = qena[i];
+      opt.innerHTML = qena[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Luxor"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< luxor.length; i++){
+      var opt = document.createElement('option');
+      opt.value = luxor[i];
+      opt.innerHTML = luxor[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Aswan"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< aswan.length; i++){
+      var opt = document.createElement('option');
+      opt.value = aswan[i];
+      opt.innerHTML = aswan[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Matruh"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< matroh.length; i++){
+      var opt = document.createElement('option');
+      opt.value = matroh[i];
+      opt.innerHTML = matroh[i];
+      sel.appendChild(opt);
+    }
+  }
+  if(city == "Dakahlia"){
+    sel.innerHTML = "";
+    var op = document.createElement('option');
+    op.value = "";
+      op.innerHTML = "";
+      sel.appendChild(op);
+    for (var i = 0; i< daka.length; i++){
+      var opt = document.createElement('option');
+      opt.value = daka[i];
+      opt.innerHTML = daka[i];
+      sel.appendChild(opt);
+    }
+  }
+}
 function sendM(email, t){
         Email.send({
             Host: "smtp.gmail.com",
@@ -38,7 +414,7 @@ function getch(id){
   function checkValidity(){
     var title = document.getElementById("title").value;
     var desc = document.getElementById("comments").value;
-    var loc = document.getElementById("loc").value;
+    var loc = document.getElementById("locations").value;
     var city = document.getElementById("cities").value;
     var flr = document.getElementById("floor").value;
     var bed = document.getElementById("bed").value;
@@ -230,7 +606,7 @@ function getch(id){
       
   var title = document.getElementById("title").value;
   var desc = document.getElementById("comments").value;
-  var loc = document.getElementById("loc").value;
+  var loc = document.getElementById("locations").value;
   var city = document.getElementById("cities").value;
   var flr = document.getElementById("floor").value;
   var bed = document.getElementById("bed").value;
@@ -329,4 +705,5 @@ function getch(id){
     document.getElementById("bo").appendChild(main);
   }
 document.getElementById("add2").addEventListener("click", checkValidity);
+document.getElementById("cities").addEventListener("change", setLoc);
 //document.getElementById("setActive").addEventListener("click", makeLoader);
