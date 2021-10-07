f9791620e620ac56454579fa4ec948792cb7596b
Kerollos <kcok2014@gmail.com>
2021-10-08 00:17:29 +0200

    Kerollos

--- Front/showNum.js
+++ Front/showNum.js
@@ -2,5 +2,6 @@ function show (){
     document.getElementById("ccc").style.display = "block";
     document.getElementById("num").style.display = "block";
     document.getElementById("showNum").style.display = "none";
+    document.getElementById("Whats").style.display = "block";
 }
 document.getElementById("showNum").addEventListener("click", show);
